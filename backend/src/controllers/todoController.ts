bd84638965c5c5d286f267f8c7cedd81cf0bdce7
Aldo Omar Andres <aldoandres045@gmail.com>
2025-08-31 11:49:44 -0300

    add docker-compose for local env

--- backend/src/controllers/todoController.ts
+++ backend/src/controllers/todoController.ts
@@ -2,7 +2,16 @@ import { Request, Response } from "express";
 import { v4 as uuidv4 } from "uuid";
 import Redis from "ioredis";
 
-const client = new Redis(process.env.REDIS_URL + "");
+let client: Redis;
+if (process.env.LOCAL || false) {
+  client = new Redis({
+    host: process.env.REDIS_HOST || "localhost",
+    port: Number(process.env.REDIS_PORT) || 6379,
+    
+  });
+} else {
+  client = new Redis(process.env.REDIS_URL + "");
+}
 
 export const getTasks = async (_req: Request, res: Response) => {
   const redisClient = client;
